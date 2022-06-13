32325ac56e2faa8f9b57e1e0930881c67f054dcd
anastasiiakorhsina <anastasiiakorshina@gmail.com>
2022-06-13 14:16:14 +0300

    Initial Commit

---
+++ TarotAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TarotAppSwiftUI
+//
+//  Created by Anastasiia Korshina on 13.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TarotAppSwiftUI/TarotAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotAppSwiftUIApp.swift
+//  TarotAppSwiftUI
+//
+//  Created by Anastasiia Korshina on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TarotAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
